4557b4fbcda82f7d2cc32a408bb4d2b108c56e66
hoang <107757542+hkhanhdev@users.noreply.github.com>
2024-05-07 20:45:45 +0700

    completed user profile page

--- resources/views/livewire/pages/client/profile.blade.php
+++ resources/views/livewire/pages/client/profile.blade.php
@@ -3,25 +3,70 @@
 use Livewire\Volt\Component;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
+use Illuminate\Validation\Rule;
 
 new #[Layout("components.layouts.guest")]
 #[Title("Profile")]
 class extends Component {
     //
+    use \Mary\Traits\Toast;
     public $full_location = ['display'=>'Profile','route'=>'profile','icon_name'=>"o-user"];
-//    public string $group = 'group2';
+
     public bool $logoutModal = false;
+    public bool $editModal = false;
+    public $current_user_id;
+    public $name;
+    public $email;
+    public $phone;
+    public $address;
+    public $createdAt;
+
+    protected $rules = [];
+
+    public function validateCredentials()
+    {
+        $this->editModal = false;
+        $this->setRules();
+        $validated = $this->validate($this->rules);
+        $user = \App\Models\User::find($this->current_user_id);
+        $user->update($validated);
+        sleep(1);
+        $this->redirect(route("client_profile"));
+        $this->success("Updated!",position: 'toast-top toast-end');
+    }
+    protected function setRules()
+    {
+        $this->rules = [
+            'name' => ['required', 'string', 'max:50','min:1'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255',Rule::unique('users', 'email')->ignore($this->current_user_id)],
+            'address' => ['string', 'max:255'],
+            'phone' =>['required','regex:/^(?:\+84|0)?[1-9]\d{8,9}$/',Rule::unique('users', 'phone')->ignore($this->current_user_id)],
+        ];
+    }
     public function getUserInfo()
     {
-        dd(auth()->user()->orders->where('status','!=','in cart'));
-        return auth()->user();
+        $current_user = auth()->user();
+        $this->createdAt = $this->getTime($current_user->created_at);
+        $this->current_user_id = $current_user->id;
+        $this->name = $current_user->name;
+        $this->email = $current_user->email;
+        $this->phone = $current_user->phone;
+        $this->address = $current_user->address;
+        return $current_user;
     }
 
+    protected function getTime($created_time)
+    {
+        $createdAt = \Carbon\Carbon::parse($created_time);
+        $now = \Carbon\Carbon::now();
+        $timeDifference = $createdAt->diffForHumans($now);
+        return $timeDifference;
+    }
     public function with():array
     {
         return [
-            'user_info' => auth()->user(),
-//            'user_info' => $this->getUserInfo(),
+//            'user_info' => auth()->user(),
+            'user_info' => $this->getUserInfo(),
             'orders_info' => auth()->user()->orders->where('status','!=','in cart')
         ];
     }
@@ -41,7 +86,7 @@ class extends Component {
     <x-gap/>
     <livewire:partials.bread-crumb display="{{$full_location['display']}}" route="{{$full_location['route']}}" icon_name="{{$full_location['icon_name']}}"/>
     <div class="card bg-base-100 shadow-xl w-9/12">
-        <div class="flex w-full" x-data="{ order: true,edit:false }">
+        <div class="flex w-full" x-data="{ order: false,edit:true }">
             <div class="card bg-base-100 p-10">
                 <div class="flex flex-col items-center">
                     <img src="https://th.bing.com/th/id/OIP.kcaJsnMsMsFRdU6d1m2v6AHaHa?w=194&h=194&c=7&r=0&o=5&pid=1.7" class="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0">
@@ -58,7 +103,7 @@ class extends Component {
                         <button class="flex gap-1 items-center bg-primary hover:scale-105 duration-300 py-2 px-2 rounded" @click="order = false,edit=true">
                             <x-ui-icon name="o-pencil-square"></x-ui-icon>
                             Edit Profile</button>
-                        <button class="flex gap-1 items-center bg-red-500 hover:scale-105 duration-300 py-2 px-7 rounded" @click="$wire.logoutModal = true">
+                        <button class="flex gap-1 items-center bg-error hover:scale-105 duration-300 py-2 px-7 rounded" @click="$wire.logoutModal = true">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
                             </svg>
@@ -69,17 +114,22 @@ class extends Component {
                 <div class="flex flex-col">
                     <span class="uppercase font-bold tracking-wider mb-2">More Infomation</span>
                     <ul>
-                        <li class="mb-2">Joined for</li>
+                        <li class="mb-2">Joined {{$createdAt}}</li>
+                        @if($user_info->role == '0')
+                            <li class="mb-2">Role:User/Customer</li>
+                        @else
+                            <li class="mb-2">Role:Administrator</li>
+                        @endif
                     </ul>
                 </div>
             </div>
             <div class="divider divider-horizontal"></div>
             <div class="flex card w-10/12" x-show="order" x-transition:enter="transition ease-out duration-300"
-                 x-transition:enter-start="opacity-10 transform scale-90"
-                 x-transition:enter-end="opacity-100 transform scale-100"
-                 x-transition:leave="transition ease-in duration-200"
-                 x-transition:leave-start="opacity-100 transform scale-100"
-                 x-transition:leave-end="opacity-10 transform scale-90">
+                 x-transition:enter-start="opacity-0 scale-90"
+                 x-transition:enter-end="opacity-100 scale-100"
+                 x-transition:leave="transition ease-in duration-300"
+                 x-transition:leave-start="opacity-100 scale-100"
+                 x-transition:leave-end="opacity-0 scale-90">
 
                 <div class="px-3">
                     <div class="flex flex-col">
@@ -115,23 +165,34 @@ class extends Component {
                 </div>
             </div>
             <div class="flex card w-10/12 p-3" x-show="edit" x-transition:enter="transition ease-out duration-300"
-                 x-transition:enter-start="opacity-0 transform scale-90"
-                 x-transition:enter-end="opacity-100 transform scale-100"
-                 x-transition:leave="transition ease-in duration-200"
-                 x-transition:leave-start="opacity-100 transform scale-100"
-                 x-transition:leave-end="opacity-0 transform scale-90">
+                 x-transition:enter-start="opacity-0 scale-90"
+                 x-transition:enter-end="opacity-100 scale-100"
+                 x-transition:leave="transition ease-in duration-300"
+                 x-transition:leave-start="opacity-100 scale-100"
+                 x-transition:leave-end="opacity-0 scale-90">
                 <div class="flex flex-col">
                     <span class="font-semibold text-2xl">Edit profile</span>
                     <span class="">Update your details information</span>
                 </div>
 
-                <hr class="my-3 border-t border-gray-300">
-                <div class="card card-side bg-base-100 shadow-sm">
+                <hr class="my-2 border-t border-gray-300">
+                <div class="card card-side">
+                    <x-ui-modal wire:model="editModal" title="Are you sure?" subtitle="We'll update your profile" separator>
+                        <div>Make sure your provided credentials are correct!</div>
 
-                    <div class="card-body ">
-                        <span>Order Details</span>
+                        <x-slot:actions>
+                            <x-ui-button label="Cancel" @click="$wire.editModal = false" />
+                            <x-ui-button label="Confirm" class="btn-primary" wire:click="validateCredentials" spinner/>
+                        </x-slot:actions>
+                    </x-ui-modal>
+
+                    <div class="card-body gap-6">
+                        <x-ui-input label="Username" icon="o-user" wire:model="name"/>
+                        <x-ui-input label="Email" icon="o-envelope" wire:model="email"/>
+                        <x-ui-input label="Phone"  icon="o-phone" wire:model="phone"/>
+                        <x-ui-input label="Address"  icon="o-map-pin" wire:model="address"/>
                         <div class="card-actions justify-end">
-                            <button class="btn btn-primary">Save</button>
+                            <button class="btn btn-primary" @click="$wire.editModal = true">Save</button>
                         </div>
                     </div>
                 </div>
