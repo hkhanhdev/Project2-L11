b35d57a76511c9d107ff30139df91c2af898e31e
hoang <107757542+hkhanhdev@users.noreply.github.com>
2024-05-07 22:41:20 +0700

    completed

--- resources/views/livewire/pages/admin/users_management.blade.php
+++ resources/views/livewire/pages/admin/users_management.blade.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Collection;
 use Livewire\WithPagination;
 use Livewire\WithoutUrlPagination;
 use Livewire\Attributes\{Layout, Title};
+use Illuminate\Validation\Rule;
 
 new
 #[Layout('components.layouts.admin')]
@@ -13,17 +14,18 @@ new
 class extends Component {
     use Toast, WithPagination, WithoutUrlPagination;
 
-    protected $rules = [
-        'name' => ['required', 'string', 'max:255'],
-        'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.\App\Models\User::class],
-        'password' => ['required', 'string'],
+    protected $edit_rules = [
         'role' => [],
         'address' => ['required','string'],
-        'phone' =>['required','regex:/^(?:\+84|0)?[1-9]\d{8,9}$/', 'unique:'.\App\Models\User::class],
-
+        'name' => ['required', 'string', 'max:80']
     ];
+
+    protected function setRules($user_id)
+    {
+        $this->edit_rules['email'] = ['required', 'string', 'lowercase', 'email', 'max:255',Rule::unique('users', 'email')->ignore($user_id)];
+        $this->edit_rules['phone'] = ['required','regex:/^(?:\+84|0)?[1-9]\d{8,9}$/',Rule::unique('users', 'phone')->ignore($user_id)];
+    }
     public string $search = '';
-    public bool $add_drawer = false;
     public bool $edit = false;
 
     public $user_id;
@@ -34,44 +36,19 @@ class extends Component {
     public $phone;
     public $role = '0';
 
-    // Clear filters
-    public function clear(): void
-    {
-        $this->reset();
-        $this->success('Filters cleared.', position: 'toast-bottom', timeout: 4000);
-    }
-
     public function saveEdit(): void
     {
-        $validated = $this->validate($this->rules);
-        dd($validated);
+        $this->setRules($this->user_id);
+        $validated = $this->validate($this->edit_rules);
+        $user = \App\Models\User::find($this->user_id);
+        $user->update($validated);
         $this->success('Updated', position: 'toast-bottom', timeout: 4000);
-
-        $this->error('Something is wrong.Please try again!', position: 'toast-bottom', timeout: 4000);
-
     }
 
-    public function saveAdd(): void
+    public function closeModal()
     {
-        $validated = $this->validate($this->rules);
-        $validated['password'] = \Illuminate\Support\Facades\Hash::make($validated['password']);
-        $user = \App\Models\User::create($validated);
         $this->reset();
-        $this->success('User added successfully!', position: 'toast-bottom', timeout: 4000);
     }
-
-    // Delete action
-    public function delete($id): void
-    {
-
-        $this->reset();
-        $this->warning('Cannot delete associated product,change product status to hidden instead', position: 'toast-bottom', timeout: 6000);
-
-        $this->reset();
-        $this->error('Something is wrong.Please try again!', position: 'toast-bottom', timeout: 4000);
-
-    }
-
     public function openEditModal($user)
     {
         $this->user_id = $user['id'] ?? '';
@@ -79,7 +56,6 @@ class extends Component {
         $this->email = $user['email'] ?? '';
         $this->address = $user['address'] ?? '';
         $this->phone = $user['phone'] ?? '';
-//        dd($user);
         $this->edit = true;
     }
 
@@ -92,8 +68,7 @@ class extends Component {
             ['key' => 'email', 'label' => 'Email', 'class' => 'w-64 text-primary'],
             ['key' => 'role', 'label' => 'Privilege', 'class' => 'w-36 text-primary'],
             ['key' => 'address', 'label' => 'Address', 'class' => 'w-48 text-primary'],
-            ['key' => 'phone', 'label' => 'Phone', 'class' => 'w-20 text-primary'],
-            ['key' => 'status', 'label' => 'Status', 'class' => 'w-36 text-primary']
+            ['key' => 'phone', 'label' => 'Phone', 'class' => 'w-20 text-primary']
         ];
     }
 
@@ -118,34 +93,9 @@ class extends Component {
         <x-slot:middle class="!justify-end">
             <x-ui-input placeholder="User name..." wire:model.live.debounce="search" clearable icon="o-magnifying-glass"/>
         </x-slot:middle>
-        <x-slot:actions>
-            <x-ui-button icon="o-plus" class="btn-primary" label="Add" @click="$wire.add_drawer = true"/>
-        </x-slot:actions>
+
     </x-ui-header>
 
-    {{--        ADD DRAWER--}}
-    <x-ui-drawer wire:model="add_drawer" title="Add new user" right separator with-close-button class="lg:w-1/3">
-        <x-ui-form wire:submit="saveAdd">
-            <x-ui-input label="Username" wire:model="name"/>
-            <x-ui-input label="Email" wire:model="email"/>
-            <x-ui-input label="Password" wire:model="password" type="password"/>
-            <x-ui-input label="Address" wire:model="address"/>
-            <x-ui-input label="Phone" wire:model="phone"/>
-            <label class="form-control w-full max-w-xs">
-                <div class="label">
-                    <span class="label-text">Role</span>
-                </div>
-                <select class="select select-bordered select-info" wire:model="role">
-                    <option value="0" selected>User</option>
-                    <option value="1">Admin</option>
-                </select>
-            </label>
-            <x-slot:actions>
-                <x-ui-button label="Cancel" icon="o-x-mark" @click="$wire.add_drawer = false" spinner/>
-                <x-ui-button label="Save" icon="o-check" class="btn-primary" type="submit" spinner="saveAdd"/>
-            </x-slot:actions>
-        </x-ui-form>
-    </x-ui-drawer>
     <x-ui-card>
         <x-ui-table :headers="$headers" :rows="$users" with-pagination class="table-md">
 
@@ -157,18 +107,22 @@ class extends Component {
                 @endif
             @endscope
             @scope('actions', $user)
-            <x-ui-button icon="o-pencil-square" spinner class="btn-sm btn-warning"
-                         @click="$wire.openEditModal({{$user}})"/>
+            <div>
+                <x-ui-button icon="o-pencil-square" spinner class="btn-sm btn-warning"
+                             @click="$wire.openEditModal({{$user}})"/>
+            </div>
             @endscope
         </x-ui-table>
     </x-ui-card>
 
     {{--    edit modal--}}
-    <x-ui-modal wire:model="edit" title="Edit panel" subtitle="Change product infomation">
+    <x-ui-modal wire:model="edit" title="Edit user panel" subtitle="Change user infomation" persistent>
         <x-ui-form wire:submit="saveEdit">
             <x-ui-input label="UserID" disabled value="{{$user_id ?? ''}}"/>
             <x-ui-input label="Username" wire:model="name"/>
             <x-ui-input label="Address" wire:model="address"/>
+            <x-ui-input label="Email" wire:model="email"/>
+            <x-ui-input label="Phone" wire:model="phone"/>
             <label class="form-control w-full max-w-xs">
                 <div class="label">
                     <span class="label-text">Role</span>
@@ -180,7 +134,7 @@ class extends Component {
             </label>
 
             <x-slot:actions>
-                <x-ui-button label="Cancel" @click="$wire.edit = false"/>
+                <x-ui-button label="Cancel" @click="$wire.closeModal()" spinner="closeModal"/>
                 <x-ui-button label="Save" class="btn-success" type="submit" spinner="save"/>
             </x-slot:actions>
         </x-ui-form>
