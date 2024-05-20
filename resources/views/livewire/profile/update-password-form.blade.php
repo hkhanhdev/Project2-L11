f6b310d148959c91ce4869d4a946432137fc99ac
hoang <nguyenxuanhakhanh88060@gmail.com>
2024-05-20 11:19:00 +0700

    optimized

--- resources/views/livewire/profile/update-password-form.blade.php
+++ resources/views/livewire/profile/update-password-form.blade.php
@@ -51,26 +51,19 @@ new class extends Component
 
     <form wire:submit="updatePassword" class="mt-6 space-y-6">
         <div>
-            <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-            <x-text-input wire:model="current_password" id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
-            <x-input-error :messages="$errors->get('current_password')" class="mt-2" />
+            <x-ui-input label="Current password" wire:model="current_password" type="text" />
         </div>
 
         <div>
-            <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input wire:model="password" id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-ui-input label="New password" wire:model="password" type="password" />
         </div>
 
         <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input wire:model="password_confirmation" id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            <x-ui-input label="Confirm password" wire:model="password_confirmation" type="password" />
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
+            <x-ui-button label="Save" class="btn-neutral" wire:click="updatePassword"/>
             <x-action-message class="me-3" on="password-updated">
                 {{ __('Saved.') }}
             </x-action-message>
